136a389e75caa63ef432aa1d6086c837bc297f39
Thalia <thaliag@my.yorku.ca>
2022-03-04 23:03:39 -0500

    Filtering data for network

---
+++ create_network.py
@@ -0,0 +1,12 @@
+import os
+import networkx as nx
+import pandas as pd
+
+if __name__ == "__main__":
+    path = os.path.join("comment_data","coronavirus_all_comments.csv")
+    df = pd.read_csv(path)
+
+    authors_list = df['author'].tolist()
+    df = df[['author', 'id', 'link_id', 'parent_id', 'subreddit_id']]
+    
+    print(df)
