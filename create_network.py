27dc75fac9b2f1306bfebd05659ce0e0ed90ba81
Razielle Dagdag <razielledagdag@outlook.com>
2022-03-06 11:37:35 -0500

    get child and parent authors to create edge

--- create_network.py
+++ create_network.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     # delete all rows that have "[deleted]" author
     df = df[df.author != "[deleted]"]
     df = df.reset_index(drop=True)
-    graph = nx.Graph()
+    graph = nx.DiGraph()
     # graph.add_edges_from(edgelist[author, author])
     # edge = parent_id <-> id
     # node = author
@@ -25,14 +25,27 @@ if __name__ == "__main__":
     for id in df['parent_id']:
       if l <=10:
         parent_id = id.split('_', 1)[1]
-        print('p_id: {}'.format(parent_id))
-        # get all rows where parent_id = parent_id (reply to someone's a comment)
-        childs_rows = df.loc[df['parent_id'] == parent_id]
+        # print('p_id: {}'.format(parent_id))
         # check if id == parent_id
         if (df['id'] == parent_id).any():
           # get the row where id = parent_id (direct comment)
           parent_row = df.loc[df['id'] == parent_id]
-          print('c_author: {}\np_author: {}'.format(childs_rows, parent_row))
+          # get all rows where parent_id = parent_id (reply to someone's a comment)
+          child_rows = df.loc[df['parent_id'] == id]
+          # print('p_row: {}\nc_rows: {}'.format(parent_row, child_rows))
+
+          
+          # check if authors has already interacted with each other - use .has_node(author), change the weight
+          
+          # create edges and set nodes name=author
+          for index, child_row in child_rows.iterrows():
+            # (source = child_author, target = parent_author, weight)
+            child_author = child_row['author']
+            parent_author = parent_row['author']
+            print('p_author: {}\nc_author: {}'.format(parent_author, child_author))
+            # graph.add_weighted_edges_from([()])
+
+
         l+=1
 
 
