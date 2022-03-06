5151974be4683db39e542de0a274fb0491a6b1b8
Razielle Dagdag <razielledagdag@outlook.com>
2022-03-06 10:52:55 -0500

    wip get rows that reply to a comment and row of the comment

--- create_network.py
+++ create_network.py
@@ -19,6 +19,23 @@ if __name__ == "__main__":
     # basically get matching parent_id and id, parent_id and link_id
     # df.loc[df["parent_id"] == "fnftok4", 'A']
 
+    # print(df['parent_id'])
+    
+    l = 1
+    for id in df['parent_id']:
+      if l <=10:
+        parent_id = id.split('_', 1)[1]
+        print('p_id: {}'.format(parent_id))
+        # get all rows where parent_id = parent_id (reply to someone's a comment)
+        childs_rows = df.loc[df['parent_id'] == parent_id]
+        # check if id == parent_id
+        if (df['id'] == parent_id).any():
+          # get the row where id = parent_id (direct comment)
+          parent_row = df.loc[df['id'] == parent_id]
+          print('c_author: {}\np_author: {}'.format(childs_rows, parent_row))
+        l+=1
 
-    print(df[df.author == "post_alone_musk"])
+
+
+    # print(df[df.author == "post_alone_musk"])
 
