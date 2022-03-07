0372a97b5e8f378ca0434bb02c32a1efeea5cbe8
JustinSo1 <sojustin28@gmail.com>
2022-03-07 03:53:58 -0500

    Refactor method to get reddit data to use PMAW

--- data_collection_ops/get_data.py
+++ data_collection_ops/get_data.py
@@ -1,49 +1,28 @@
-import requests
+from pmaw import PushshiftAPI
 
 
-def get_data(time_period, data_type, subreddit, **kwargs):
-    before, after = time_period.before, time_period.after
+def get_data(data_type, subreddit, **kwargs):
+    api = PushshiftAPI()
+    func_mapping = {
+        "comment": get_pushshift_comments,
+        "submission": get_pushshift_submissions
+    }
+    pushshift_data = func_mapping[data_type](api, subreddit, **kwargs)
 
-    # Quick fix for getting comments since date doesn't matter
-    if not before and not after:
-        return get_pushshift_data(data_type, subreddit=subreddit, **kwargs)
-
-    all_results = results = get_pushshift_data(
-        before=before,
-        after=after,
-        data_type=data_type,
-        subreddit=subreddit,
-        **kwargs
-    ).get("data")
-    query_iteration = 1
-    while len(results) != 0:
-        print("Query iteration: {}".format(query_iteration))
-        after_new = results[-1]["created_utc"]
-        results = get_pushshift_data(
-            after=after_new,
-            before=before,
-            data_type=data_type,
-            subreddit=subreddit,
-            **kwargs
-        ).get("data")
-
-        all_results.extend(results)
-        query_iteration += 1
-
-    return all_results
+    return pushshift_data
 
 
-def get_pushshift_data(data_type, **kwargs):
+def get_pushshift_comments(api, subreddit, **kwargs):
     """
     Gets data from the pushshift api.
-
-    data_type can be 'comment' or 'submission'
-    The rest of the args are interpreted as payload.
-
     Read more: https://github.com/pushshift/api
     """
+    return api.search_comments(subreddit=subreddit, **kwargs)
 
-    base_url = f"https://api.pushshift.io/reddit/search/{data_type}/"
-    payload = kwargs
-    request = requests.get(base_url, params=payload)
-    return request.json()
+
+def get_pushshift_submissions(api, subreddit, **kwargs):
+    """
+    Gets data from the pushshift api.
+    Read more: https://github.com/pushshift/api
+    """
+    return api.search_submissions(subreddit=subreddit, **kwargs)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import datetime
 import json
 
-import pandas as pd
 from pmaw import PushshiftAPI
 
 from data_collection import RedditData
@@ -18,38 +17,11 @@ def get_post_ids(files):
     return link_IDs
 
 
-def get_submissions_data(data):
-    # headers for submissions
-    headers = ["author", "created_utc", "awarders", "domain", "full_link", "gildings", "id", "is_crosspostable",
-               "is_original_content", "is_reddit_media_domain", "is_self", "is_video", "locked", "num_comments",
-               "num_crossposts", "over_18", "permalink", "pinned", "post_hint", "preview", "retrieved_on",
-               "score", "selftext", "stickied", "subreddit", "subreddit_id", "subreddit_subscribers",
-               "subreddit_type", "thumbnail", "title", "total_awards_received", "upvote_ratio", "url"]
-
-    # "sort_type = score" provides more results than "sort_type = num_comments"
-    reddit_data = data.get_data("submission", "coronavirus", sort_type="score", sort="desc", size=100)
-    data.write_data_to_json_file(reddit_data, "submission", "coronavirus")
-    data.write_data_to_csv(reddit_data, headers, "submission", "coronavirus")
-    return reddit_data
-
-
-def get_comments_data(data):
-    # Cannot sort comments by number of comments (makes sense but documentation is wrong)
-    reddit_data = data.get_data("comment", "coronavirus", sort_type="score", sort="desc", size=100)
-    # headers = ["author", "author_flair_css_class", "author_flair_text", "body", "created_utc",
-    #            "edited", "id", "is_submitter", "link_id", "parent_id", "retrieved_on", "score",
-    #            "stickied", "subreddit", "subreddit_id"]
-    # data.write_data_to_csv(reddit_data, headers, "comment", "coronavirus")
-    return reddit_data
-
-
-def get_comments_from_submission(subreddit, **kwargs):
-    api = PushshiftAPI()
-
+def get_comments_from_submission(api, link_IDs, subreddit, **kwargs):
     all_comments = []
     for idx, post_id in enumerate(link_IDs):
         print(f"Query iteration: {idx + 1}")
-        comments = api.search_comments(subreddit=subreddit, link_id=post_id, **kwargs)
+        comments = api.get_data(data_type="comment", subreddit=subreddit, link_id=post_id, **kwargs)
         comments_list = [comment for comment in comments]
         all_comments.extend(comments_list)
 
@@ -58,21 +30,31 @@ def get_comments_from_submission(subreddit, **kwargs):
 
 if __name__ == '__main__':
     before = int(datetime.datetime(year=2020, month=12, day=31).timestamp())
-    after = int(datetime.datetime(year=2020, month=1, day=1).timestamp())
-    data = RedditData(before, after, "test_directory")
+    after = int(datetime.datetime(year=2020, month=1, day=0).timestamp())
+    directory = "test_directory"
+    data = RedditData("test_directory")
 
-    # reddit_submissions = get_submissions_data(data)
+    headers = ["author", "created_utc", "awarders", "domain", "full_link", "gildings", "id", "is_crosspostable",
+               "is_original_content", "is_reddit_media_domain", "is_self", "is_video", "locked", "num_comments",
+               "num_crossposts", "over_18", "permalink", "pinned", "post_hint", "preview", "retrieved_on",
+               "score", "selftext", "stickied", "subreddit", "subreddit_id", "subreddit_subscribers",
+               "subreddit_type", "thumbnail", "title", "total_awards_received", "upvote_ratio", "url"]
 
-    # This line is now redundant due to using PMAW API but kept just in case for testing purposes
-    # reddit_comments = get_comments_data(data)
+    reddit_submissions = data.get_data(subreddit="coronavirus", data_type="submission", sort_type="score", sort="desc",
+                                       before=before, after=after)
+    submission_list = [submission for submission in reddit_submissions]
+    print(f"{len(submission_list)} submissions retrieved")
 
-    files = ['./test_directory/coronavirus_submission.json']
-    link_IDs = get_post_ids(files)
-    print(len(link_IDs))
+    data.write_data_to_csv(submission_list, headers, "submission", "coronavirus")
+    # data.write_data_to_json_file(reddit_data, "submission", "coronavirus")
 
-    all_comments = get_comments_from_submission("coronavirus", sort_type="score", sort="desc")
-
-    headers = ["author", "link_id", "id", "parent_id", "created_utc", "is_submitter", "score", "subreddit",
-               "subreddit_id", "body"]
-
-    data.write_data_to_csv(all_comments, headers, "comment", "coronavirus")
+    # files = ['./test_directory/coronavirus_submission.json']
+    # link_IDs = get_post_ids(files)
+    # # print(len(link_IDs))
+    #
+    # all_comments = get_comments_from_submission(api=data, link_IDs=link_IDs, subreddit="coronavirus", sort_type="score",
+    #                                             sort="desc")
+    # headers = ["author", "link_id", "id", "parent_id", "created_utc", "is_submitter", "score", "subreddit",
+    #            "subreddit_id", "body"]
+    #
+    # data.write_data_to_csv(all_comments, headers, "comment", "coronavirus")
