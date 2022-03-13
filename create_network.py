812b2b5684b25fc74e110ac3344d3738814c9372
Razielle Dagdag <razielledagdag@outlook.com>
2022-03-13 03:22:57 -0400

    refactor code to adhere to justin's requests (clearer naming conventions, removing unnecessary code)

--- create_network.py
+++ create_network.py
@@ -4,19 +4,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from network_analysis import get_network_analysis 
 
-def set_interaction(id, parent_id, coc_df, post_df, has_interaction):
+def set_interaction(id, parent_id, comments_df, posts_df, has_interaction):
   # check for comments replying to a comment (parent_id = id)
-  if (coc_df['id'] == parent_id).any():
+  if (comments_df['id'] == parent_id).any():
     # get the row where id = parent_id (user receiving the comment)
-    parent_row = coc_df.loc[coc_df['id'] == parent_id]
+    parent_row = comments_df.loc[comments_df['id'] == parent_id]
     # get all rows where parent_id = parent_id (reply to someone's a comment)
-    child_rows = coc_df.loc[coc_df['parent_id'] == id]
+    child_rows = comments_df.loc[comments_df['parent_id'] == id]
   # check for direct comments on a post (parent_id = id)
-  elif (post_df['id'] == parent_id).any():
+  elif (posts_df['id'] == parent_id).any():
     # get the row where id = parent_id post
-    parent_row = post_df.loc[post_df['id'] == parent_id]
+    parent_row = posts_df.loc[posts_df['id'] == parent_id]
     # get all rows where parent_id = parent_id (reply to someone's a comment)s
-    child_rows = coc_df.loc[coc_df['parent_id'] == id]
+    child_rows = comments_df.loc[comments_df['parent_id'] == id]
   else:
     # node has no data for parent_id = id, hence don't create edge
     has_interaction = False
@@ -45,45 +45,42 @@ def create_graph():
   plt.show()
   plt.clf()  # clear canvas to show most recent graph
 
-def filter_coc_df(coc_df):
+def filter_comments_df(comments_df):
   # delete all rows that have "[deleted]" and "AutoModerator" author
-  coc_df = coc_df[~coc_df.author.isin(['[deleted]', 'AutoModerator'])]
-  coc_df = coc_df.reset_index(drop=True)
-  return coc_df
+  comments_df = comments_df[~comments_df.author.isin(['[deleted]', 'AutoModerator'])]
+  comments_df = comments_df.reset_index(drop=True)
+  return comments_df
 
 def get_df():
   coc_path = os.path.join("comment_data", "coronavirus_all_comments.csv")
-  coc_df = pd.read_csv(coc_path)
-  coc_df = coc_df[['author', 'id', 'link_id', 'parent_id', 'subreddit_id']]
+  comments_df = pd.read_csv(coc_path)
+  comments_df = comments_df[['author', 'id', 'link_id', 'parent_id', 'subreddit_id']]
 
   post_path = os.path.join("submission_data", "coronavirus_submissions.csv")
-  post_df = pd.read_csv(post_path)
-  post_df = post_df[['author', 'id']]
+  posts_df = pd.read_csv(post_path)
+  posts_df = posts_df[['author', 'id']]
 
-  return coc_df, post_df
+  return comments_df, posts_df
 
 if __name__ == "__main__":
-  # get 'Comment On Comment Dataframe' and 'Post Dataframe'
-  coc_df, post_df = get_df()
-  authors_list = coc_df['author'].tolist()
+  # get 'Comments Data frame' and 'Posts Data frame'
+  comments_df, posts_df = get_df()
+  authors_list = comments_df['author'].tolist()
 
-  coc_df = filter_coc_df(coc_df)
+  comments_df = filter_comments_df(comments_df)
   graph = nx.DiGraph()
   
-  # l = 1
   print("Creating edges...")
-  for id in coc_df['parent_id']:
-    # if l <= 10:
-      parent_id = id.split('_', 1)[1]
-      has_interaction = True
-      parent_row, child_rows, has_interaction = set_interaction(id, parent_id,
-                                                                coc_df, post_df,
-                                                                has_interaction)
+  for id in comments_df['parent_id']:
+    parent_id = id.split('_', 1)[1]
+    has_interaction = True
+    parent_row, child_rows, has_interaction = set_interaction(id, parent_id,
+                                                              comments_df, posts_df,
+                                                              has_interaction)
 
-      if has_interaction:
-        # create edges and set nodes name=author
-        create_edges(parent_row, child_rows)
-      # l += 1
+    if has_interaction:
+      # create edges and set nodes name=author
+      create_edges(parent_row, child_rows)
   print("Edges created!")
   
   # draw and show graph after adding edges 
--- network_analysis.py
+++ network_analysis.py
@@ -14,27 +14,26 @@ def get_network_analysis(graph):
   dia, num_nodes_strong, num_edges_strong = get_diameter(graph) # Diameter
 
   with open('network_analysis.csv', 'w', newline='') as csvfile:
-      writer = csv.writer(csvfile)
-      component_type = ['All Component', 'Weakly Connected Component', 'Strongly Connected Component']
-      
-      ac_header = ['Num of Nodes', 'Num of Edges','Global Clustering Coefficient']
-      ac_data = [num_nodes, num_edges, avg_cc]
-      writer.writerow([component_type[0]])
-      writer.writerow(ac_header)
-      writer.writerow(ac_data)
+    writer = csv.writer(csvfile)
+    component_type = ['All Components', 'Weakly Connected Component', 'Strongly Connected Component']
+    
+    ac_header = ['Num of Nodes', 'Num of Edges','Global Clustering Coefficient']
+    ac_data = [num_nodes, num_edges, avg_cc]
+    writer.writerow([component_type[0]])
+    writer.writerow(ac_header)
+    writer.writerow(ac_data)
 
-      wcc_header = ['Num of Nodes', 'Num of Edges', 'Average Shortest Path']
-      wcc_data = num_nodes_weak, num_edges_weak, avg_sp
-      writer.writerow([component_type[1]])
-      writer.writerow(wcc_header)
-      writer.writerow(wcc_data)
+    wcc_header = ['Num of Nodes', 'Num of Edges', 'Average Shortest Path']
+    wcc_data = num_nodes_weak, num_edges_weak, avg_sp
+    writer.writerow([component_type[1]])
+    writer.writerow(wcc_header)
+    writer.writerow(wcc_data)
 
-      scc_header = ['Num of Nodes', 'Num of Edges', 'Diameter']
-      scc_data = num_nodes_strong, num_edges_strong, dia
-      writer.writerow([component_type[2]])
-      writer.writerow(scc_header)
-      writer.writerow(scc_data)
-
+    scc_header = ['Num of Nodes', 'Num of Edges', 'Diameter']
+    scc_data = num_nodes_strong, num_edges_strong, dia
+    writer.writerow([component_type[2]])
+    writer.writerow(scc_header)
+    writer.writerow(scc_data)
 
 def create_graph_analysis(x, y, title, xlabel, ylabel, file_name):
   plt.scatter(x, y)
@@ -45,46 +44,43 @@ def create_graph_analysis(x, y, title, xlabel, ylabel, file_name):
   plt.savefig(file_name)
   plt.clf() 
 
-def get_x(values):
-  set_list = list(set(values)) # remove duplicates
-  x = [v for v in set_list] 
-  return x
+def get_x_axis_values(values):
+  x_axis_values = list(set(values)) # remove duplicates
+  return x_axis_values
 
-def get_y(values):
+def get_y_axis_values(values):
   # count frequency and calculate log for y-axis
-  set_list = list(set(values)) # remove duplicates
-  y = [values.count(v) for v in set_list]
-  return y
+  y_axis_values = [values.count(v) for v in get_x_axis_values(values)]
+  return y_axis_values
 
 def get_node_degree_distribution(graph):
   # the degree is the sum of the edge weights adjacent to the node.
   # all degrees
   d = [k for n, k in graph.degree(weight='weight')]
-  create_graph_analysis(get_x(d), get_y(d), "Degree Distribution", "Degree, k", "Count, Nk", "graphs/dd_all.png")
+  create_graph_analysis(get_x_axis_values(d), get_y_axis_values(d), "Degree Distribution", "Degree, k", "Count, Nk", "graphs/dd_all.png")
 
-  # in degree
+  # in-degrees
   ind = [k for n, k in graph.in_degree(weight='weight')]
-  create_graph_analysis(get_x(ind), get_y(ind), "In Degree Distribution", "In Degree, k", "Count, Nk", "graphs/dd_in.png")
+  create_graph_analysis(get_x_axis_values(ind), get_y_axis_values(ind), "In Degree Distribution", "In Degree, k", "Count, Nk", "graphs/dd_in.png")
 
-  # out degree
+  # out-degrees
   outd = [k for n, k in graph.out_degree(weight='weight')]
-  create_graph_analysis(get_x(outd), get_y(outd), "Out Degree Distribution", "Out Degree, k", "Count, Nk", "graphs/dd_out.png")
+  create_graph_analysis(get_x_axis_values(outd), get_y_axis_values(outd), "Out Degree Distribution", "Out Degree, k", "Count, Nk", "graphs/dd_out.png")
 
 def get_weakly_connected_component(graph):
   # create subgraphs for weakly connected components
   subgraphs = [graph.subgraph(wcc) for wcc in nx.weakly_connected_components(graph)]
   # count size of subgraphs
   sizes = [g.number_of_nodes() for g in subgraphs]
-  create_graph_analysis(get_x(sizes), get_y(sizes), "Connectivity", "Weakly connected component size", "Count", "graphs/wcc.png")
+  create_graph_analysis(get_x_axis_values(sizes), get_y_axis_values(sizes), "Connectivity", "Weakly connected component size", "Count", "graphs/wcc.png")
 
 def get_clustering_coefficient(graph):
-  # get clustering coefficient for each node in graph
-  # the edge attribute that holds the numerical value used as a weight
+  # get clustering coefficient and degree for each node in graph
   d = [k for n, k in graph.degree(weight='weight')]
   cc = [v for n, v in nx.clustering(graph, weight='weight').items()]
   create_graph_analysis(d, cc, "Clustering", "Degree, k", "Clustering coefficient, c", "graphs/cc.png")
 
-  # calculate global clustering coefficient (global)
+  # calculate global clustering coefficient (average)
   avg_cc = nx.average_clustering(graph)
   return avg_cc
 
@@ -95,29 +91,19 @@ def get_shortest_path_length(graph):
     paths = nx.single_source_dijkstra_path_length(graph, u, weight='weight')
     for p in paths:
       lengths.append(paths[p])
-  create_graph_analysis(get_x(lengths), get_y(lengths), "Shortest Path", "Shortest Path Length", "Count", "graphs/sp.png")
+  create_graph_analysis(get_x_axis_values(lengths), get_y_axis_values(lengths), "Shortest Path", "Shortest Path Length", "Count", "graphs/sp.png")
   
   # calculate average shortest path length
-  try:
-    giant_component = graph.subgraph(max(nx.weakly_connected_components(graph), key=len))
-    num_nodes = giant_component.number_of_nodes()
-    num_edges = giant_component.number_of_edges()
-    avg_sp = nx.average_shortest_path_length(giant_component, weight='weight', method='dijkstra')
-  except Exception as e:
-    avg_sp = e
-    num_nodes = e
-    num_edges = e
+  giant_component = graph.subgraph(max(nx.weakly_connected_components(graph), key=len))
+  num_nodes = giant_component.number_of_nodes()
+  num_edges = giant_component.number_of_edges()
+  avg_sp = nx.average_shortest_path_length(giant_component, weight='weight', method='dijkstra')
   return avg_sp, num_nodes ,num_edges
 
 def get_diameter(graph):
   # calculate diameter
-  try:
-    giant_component = graph.subgraph(max(nx.strongly_connected_components(graph), key=len))
-    num_nodes = giant_component.number_of_nodes()
-    num_edges = giant_component.number_of_edges()
-    dia = nx.diameter(giant_component)
-  except Exception as e:
-    dia = e
-    num_nodes = e
-    num_edges = e
+  giant_component = graph.subgraph(max(nx.strongly_connected_components(graph), key=len))
+  num_nodes = giant_component.number_of_nodes()
+  num_edges = giant_component.number_of_edges()
+  dia = nx.diameter(giant_component)
   return dia, num_nodes, num_edges
