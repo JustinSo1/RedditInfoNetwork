994a08981cba12c45e0814f31bc6c9429f8ac0c0
JustinSo1 <sojustin28@gmail.com>
2022-04-02 04:38:21 -0400

    Include POS tags in preprocessing

--- topic_modelling.py
+++ topic_modelling.py
@@ -1,13 +1,18 @@
 import os
+from itertools import chain  # to flatten list of sentences of tokens into list of tokens
 from pprint import pprint
 
 import gensim
 import pandas as pd
-from gensim.corpora import Dictionary
-from gensim.models import LdaModel
+from gensim import corpora
+from gensim import models
 from gensim.models import Phrases
 from gensim.models.phrases import ENGLISH_CONNECTOR_WORDS
+from nltk import pos_tag
+from nltk.corpus import wordnet, stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from tqdm import tqdm
 
 
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -40,6 +45,11 @@ def get_all_comments_by_user(df, user):
     return all_comments_by_author['body'].tolist()
 
 
+def get_all_tokens_by_user(df, user):
+    all_tokens_by_user = df[df['author'] == user]
+    return all_tokens_by_user['tokens'].tolist()
+
+
 def preprocess(text):
     result = []
     for token in gensim.utils.simple_preprocess(text):  # preprocess_string
@@ -48,79 +58,91 @@ def preprocess(text):
     return result
 
 
-if __name__ == "__main__":
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
 
+
+if __name__ == "__main__":
     filename = os.path.join("comment_data", "coronavirus_all_comments.csv")
     documents = extract_documents(filename)
+    # nltk.download('punkt') # Run only once
+    # nltk.download('averaged_perceptron_tagger') # Run only once
+    tqdm.pandas()  # Allows progress_map to be used
+
+    # Separates sentences in comment
+    documents['sentences'] = documents['body'].progress_map(sent_tokenize)
+
+    # Tokenizes each word in the sentences
+    documents['tokens_sentences'] = documents['sentences'].progress_map(
+        lambda sentences: [word_tokenize(sentence) for sentence in sentences])
+
+    # Assigns a Part-Of-Speech tag for each token
+    documents['POS_tokens'] = documents['tokens_sentences'].progress_map(
+        lambda tokens_sentences: [pos_tag(tokens) for tokens in tokens_sentences])
+
+    lemmatizer = WordNetLemmatizer()
+
+    # Lemmatizing each word with its POS tag, in each sentence
+    documents['tokens_sentences_lemmatized'] = documents['POS_tokens'].progress_map(
+        lambda list_tokens_POS: [
+            [
+                lemmatizer.lemmatize(el[0], get_wordnet_pos(el[1]))
+                if get_wordnet_pos(el[1]) != '' else el[0] for el in tokens_POS
+            ]
+            for tokens_POS in list_tokens_POS
+        ]
+    )
+
+    # Flattens sentences array: ['ABC', 'DEF']) --> A B C D E F
+    documents['tokens'] = documents['tokens_sentences_lemmatized'].map(
+        lambda sentences: list(chain.from_iterable(sentences)))
+    documents['tokens'] = documents['tokens'].map(lambda tokens: [token.lower() for token in tokens if token.isalpha()
+                                                                  and token.lower() not in
+                                                                  stopwords.words('english') and len(token) > 1])
+
     # get top 10 most frequent names
     n = 10
     authors = documents['author'].value_counts()[:n].index.tolist()
     authors = filter_authors(authors)
+
     # authors = get_unique_authors(documents)
-    # print(f"Unique authors: {len(authors)}")
-    # most_frequent_authors = get_top_k_frequent_authors(documents, n)
-    # print(most_frequent_authors)
+    print(f"Unique authors: {len(authors)}")
 
     avg_topic_coherences = []
 
     for author in authors:
         print(author)
-        docs = get_all_comments_by_user(documents, author)
-        if len(docs) < 10:  # Get rid of users with less than 10 comments
-            continue
-        print(f"Amount of comments: {len(docs)}")
-        print(f"Sentence: {docs}")
-        docs = [preprocess(doc) for doc in docs]
-        print(f"preprocess: {docs}")
+        author_tokens = get_all_tokens_by_user(documents, author)
 
-        lemmatizer = WordNetLemmatizer()
-        docs = [[lemmatizer.lemmatize(token) for token in doc] for doc in docs]
+        # Computes Bigram and Trigrams for the tokens
+        bigram_model = Phrases(author_tokens, connector_words=ENGLISH_CONNECTOR_WORDS)
+        trigram_model = Phrases(bigram_model[author_tokens], min_count=1, connector_words=ENGLISH_CONNECTOR_WORDS)
+        author_tokens = list(trigram_model[bigram_model[author_tokens]])
 
-        print(f"lemmatized: {docs}")
+        dictionary_LDA = corpora.Dictionary(author_tokens)
+        # dictionary_LDA.filter_extremes(no_below=3)
+        corpus = [dictionary_LDA.doc2bow(tok) for tok in author_tokens]
 
-        # Add bigrams and trigrams to docs (only ones that appear 20 times or more).
-        bigram = Phrases(docs, min_count=1, threshold=1, connector_words=ENGLISH_CONNECTOR_WORDS)
-        for idx in range(len(docs)):
-            for token in bigram[docs[idx]]:
-                if '_' in token:
-                    # Token is a bigram, add to document.
-                    docs[idx].append(token)
-        print(f"bigrams: {docs}")
-        # Remove rare and common tokens.
-        # Create a dictionary representation of the documents.
-        dictionary = Dictionary(docs)
-
-        # Bag-of-words representation of the documents.
-        corpus = [dictionary.doc2bow(doc) for doc in docs]
-        print(f"corpus: {corpus}")
-        print('Number of unique tokens: %d' % len(dictionary))
+        # print(f"corpus: {corpus}")
+        print('Number of unique tokens: %d' % len(dictionary_LDA))
         print('Number of documents: %d' % len(corpus))
-
-        # Train LDA model.
-        # Set training parameters.
         num_topics = 5
-        chunksize = 2000
-        passes = 20
-        iterations = 400
-        eval_every = None  # Don't evaluate model perplexity, takes too much time.
+        passes = 4
+        lda_model = models.LdaModel(corpus, num_topics=num_topics,
+                                    id2word=dictionary_LDA,
+                                    passes=passes, alpha='auto',
+                                    eta='auto', eval_every=False)
 
-        # Make a index to word dictionary.
-        temp = dictionary[0]  # This is only to "load" the dictionary.
-        id2word = dictionary.id2token
-
-        model = LdaModel(
-            corpus=corpus,
-            id2word=id2word,
-            chunksize=chunksize,
-            alpha='auto',
-            eta='auto',
-            iterations=iterations,
-            num_topics=num_topics,
-            passes=passes,
-            eval_every=eval_every
-        )
-
-        top_topics = model.top_topics(corpus)  # , num_words=20)
+        top_topics = lda_model.top_topics(corpus)  # , num_words=20)
 
         # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
         avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
