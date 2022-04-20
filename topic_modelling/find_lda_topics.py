df0ec8076779ef5aaf05983df361bd3cf830c79f
JustinSo1 <sojustin28@gmail.com>
2022-04-19 22:22:51 -0400

    Get all topics in LDA_model

--- topic_modelling/find_lda_topics.py
+++ topic_modelling/find_lda_topics.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     lda_file_path = os.path.join(ROOT_DIR, "LDA_MODEL", "all_users_lda")
     all_users_lda_model = LdaModel.load(lda_file_path)
     topic_words = []
-    for index, topic in all_users_lda_model.show_topics(formatted=True):
-        topic_words.append(f'Topic: {index}\nWords: {topic}')
+
+    for index, topic in all_users_lda_model.show_topics(num_topics=all_users_lda_model.num_topics, formatted=False):
+        topic_words.append(f'Topic: {index}\nWords: {[w[0] for w in topic]}')
+
     with open(os.path.join(ROOT_DIR, "topic_modelling", "lda_topics.txt"), "w") as log_file:
         pprint(topic_words, log_file)
